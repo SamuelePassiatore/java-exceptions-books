f049d55d875a419929922eae0aa61d36753e55a6
SamuelePassiatore <semmisno2@protonmail.com>
2023-05-10 14:32:30 +0200

    added constructor and setters & getters in libro class

--- src/org/lessons/java/biblioteca/Libro.java
+++ src/org/lessons/java/biblioteca/Libro.java
@@ -1,5 +1,55 @@
 package org.lessons.java.biblioteca;
 
 public class Libro {
+	private String titolo;
+	private int numeroPagine;
+	private String autore;
+	private String editore;
+	
+	public Libro(String titolo, int numeroPagine, String autore, String editore){
+      
+    	setTitolo(titolo);
+    	setNumeroPagine(numeroPagine);
+    	setAutore(autore);
+    	setEditore(editore);
+    }
 
+	public String getTitolo() {
+		return titolo;
+	}
+
+	public void setTitolo(String titolo) {
+		this.titolo = titolo;
+	}
+
+	public int getNumeroPagine() {
+		return numeroPagine;
+	}
+
+	public void setNumeroPagine(int numeroPagine) {
+		this.numeroPagine = numeroPagine;
+	}
+
+	public String getAutore() {
+		return autore;
+	}
+
+	public void setAutore(String autore) {
+		this.autore = autore;
+	}
+
+	public String getEditore() {
+		return editore;
+	}
+
+	public void setEditore(String editore) {
+		this.editore = editore;
+	}
+	
+	@Override
+    public String toString() {
+        return "Titolo: " + titolo + ", Numero Pagine: " + numeroPagine + ", Autore: " + autore +
+                ", Editore: " + editore;
+    }
 }
+
